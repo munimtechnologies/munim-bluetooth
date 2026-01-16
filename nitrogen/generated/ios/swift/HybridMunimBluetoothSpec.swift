d68aa1ddcdfeb8072ec4236621bde6c251ca6a42
sheehanmunim <sheehanmunim@gmail.com>
2026-01-15 22:25:46 -0500

    fix: make spec base inherit NSObject for delegate conformance

--- nitrogen/generated/ios/swift/HybridMunimBluetoothSpec.swift
+++ nitrogen/generated/ios/swift/HybridMunimBluetoothSpec.swift
@@ -44,9 +44,11 @@ public extension HybridMunimBluetoothSpec_protocol {
 }
 
 /// See ``HybridMunimBluetoothSpec``
-open class HybridMunimBluetoothSpec_base {
+open class HybridMunimBluetoothSpec_base: NSObject {
   private weak var cxxWrapper: HybridMunimBluetoothSpec_cxx? = nil
-  public init() { }
+  public override init() {
+    super.init()
+  }
   public func getCxxWrapper() -> HybridMunimBluetoothSpec_cxx {
   #if DEBUG
     guard self is any HybridMunimBluetoothSpec else {
